f2e656f8a20c88d60fba01dec6e646444437ddc6
Mila8web <mila8web@gmail.com>
2021-02-22 23:13:36 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+let moveSize=0
+let testElement = document.getElementById('test')
+
+let buttonStart = document.getElementById('button_start')
+let buttonStop = document.getElementById('button_stop')
+let buttonReset = document.getElementById('button_reset')
+
+
+
+function move () {
+    testElement.style.marginLeft=moveSize + 'px'
+    moveSize = moveSize + 3
+}
+
+let timerId
+
+function start () {
+    buttonStart.setAttribute('disabled', 'true')
+    buttonStop.removeAttribute('disabled')
+    clearInterval(timerId)
+    timerId = setInterval(move, 50)
+}
+
+function stop () {
+    // buttonStart.removeAttribute('disabled')
+    buttonStop.setAttribute('disabled', 'true')
+    buttonReset.removeAttribute('disabled')
+    clearInterval(timerId)
+}
+
+function reset () {
+    // buttonReset.setAttribute('disabled', 'true')
+    buttonStart.removeAttribute('disabled')
+    // clearInterval(timerId)
+    stop()
+    moveSize=0
+    testElement.style.marginLeft = moveSize + 'px'
+}
+
+buttonStart.onclick = start
+buttonStop.onclick = stop
+buttonReset.onclick = reset
+    
+    
